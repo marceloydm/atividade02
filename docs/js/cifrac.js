a2b4c0bfe9a2234e24bcbf70c0c06e837dbfe602
Marcelo Younes <marceloydm@gmail.com>
2022-10-14 01:41:32 -0300

    pra mais tarde

--- docs/js/cifrac.js
+++ docs/js/cifrac.js
@@ -1,6 +1,33 @@
-// let caracteresEspeciais = "áÁàÀéÉèÈíÍìÌóÓòÒúÚùÙâÂêÊîÎôÔûÛãÃõÕýÝçÇ"
-// "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ".charCodeAt()
-// "abcdefghijklmnopqrstuvwxyzç".charCodeAt()
-// "0123456789".charCodeAt()
-// "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzáÁàÀéÉèÈíÍìÌóÓòÒúÚùÙâÂêÊîÎôÔûÛãÃõÕýÝçÇ0123456789".charCodeAt()
+function ccifra () {
+    let alterador = document.getElementById("nModificador").value;
+    let characteresG = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let characteresP = "abcdefghijklmnopqrstuvwxyz"
+    let numbers = "01234567890"
+    let text = document.getElementById("toWrite").value;
+    let index = 0
 
+    for (let i=0; i<text.length; i++) {
+        if (characteresG.includes(text[i])) {
+            index = characteresG.indexOf(text[i])
+            var x = posicaoLetra(index+alterador)
+            text = text.replace(text[i],characteresG[x])
+        }
+        else if (characteresP.includes(text[i])) {
+            index = characteresP.indexOf(text[i])
+            var x = posicaoLetra(index+alterador)
+            text = text.replace(text[i],characteresP[x])
+        }
+
+
+
+
+    }
+
+
+
+
+    console.log(x)
+    console.log(index)
+    console.log(text)
+    console.log(alterador)
+}
